415d39ddb5b0e26f3cd187d25edb1124e2bc6273
brianboyko <brian.boyko@gmail.com>
2020-07-28 04:19:59 +0100

    login button works and user is authorized

--- apps/chatrooms/src/app/components/LoginButton/LoginButton.tsx
+++ apps/chatrooms/src/app/components/LoginButton/LoginButton.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginButton: React.FC<unknown> = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, user } = useAuth0();
   const handleClick = () => loginWithRedirect();
-  return <button onClick={handleClick}>Login</button>;
+  return (
+    <>
+      <pre>{JSON.stringify({ isAuthenticated, user }, null, 2)}</pre>
+      <button onClick={handleClick}>Login</button>
+    </>
+  );
 };
 
 export default LoginButton;
